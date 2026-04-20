92af597055ed9a2c86c8db025665bc5f1e3b52f5
Aminulhoque01 <aminuhoque53@gmail.com>
2026-04-20 22:14:34 +0600

    follower router

--- src/model/auth/auth.route.ts
+++ src/model/auth/auth.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { register, login, getUserController, updateUserController } from "./auth.controller";
+import { register, login, getUserController, updateUserController, unfollowUser, followUser } from "./auth.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { upload } from "../middleware/upload";
+ 
 
 const AuthRouter = express.Router();
 
@@ -13,5 +14,8 @@ AuthRouter.get("/me", authMiddleware, getUserController);
 // Update user
 AuthRouter.put("/",   authMiddleware,
   upload.single("profileImage"), updateUserController);
+
+AuthRouter.post("/follow/:id", followUser);
+AuthRouter.post("/unfollow/:id", unfollowUser);  
 
 export default AuthRouter;
